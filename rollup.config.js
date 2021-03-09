ceeb8d7a41f9798c4d993fbb5b231f112bedb28a
王金强 <wangjinqiang@gongchang.com>
2021-03-09 14:03:58 +0800

    cache

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import strip from '@rollup/plugin-strip'
 import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
-// import typescript from 'rollup-plugin-typescript2'
 
 import filesize from 'rollup-plugin-filesize'
 import { terser } from 'rollup-plugin-terser'
